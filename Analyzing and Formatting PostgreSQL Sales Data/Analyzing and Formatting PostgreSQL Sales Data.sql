bfc93d753f51c330138c3451f83312cc3bb17edc
Ibrahim EL-Sharabasy <ibrahim.elsharabasy01@gmail.com>
2025-01-16 22:08:44 +0300

    Add files via upload

---
+++ Analyzing and Formatting PostgreSQL Sales Data/Analyzing and Formatting PostgreSQL Sales Data.sql
@@ -0,0 +1,78 @@
+-- 1. Creating the dataset 
+
+CREATE TABLE orders (
+	order_id  Text, 
+	order_date  Date, 
+	market  Text, 
+	region  Text, 
+	product_id  Text, 
+	sales  Numeric, 
+	quantity  Int, 
+	discount  Numeric, 
+	profit  Numeric
+	);
+
+CREATE TABLE products (
+	product_id  Text, 
+	category  Text, 
+	sub_category  Text, 
+	product_name  Text 
+	);
+
+-- 2. Import records from the dataset CSV source via the "pgAdmin 4" interface
+
+
+-- 3. View the data to understand its structure, column names, data types, and overall content
+
+SELECT *
+FROM orders;
+
+SELECT *
+FROM products;
+
+
+-- The First Task: top_five_products_each_category
+
+WITH top_sales AS(
+	SELECT category, 
+			product_name, ROUND(SUM(sales)::NUMERIC, 2) AS product_total_sales, 
+			ROUND(SUM(profit)::NUMERIC, 2) AS product_total_profit,
+			ROW_NUMBER() OVER(PARTITION BY category ORDER BY SUM(sales) DESC) AS product_rank
+	FROM products 
+	INNER JOIN orders
+	USING(product_id)
+	GROUP BY 1, 2)
+SELECT * 
+FROM top_sales
+WHERE (product_rank <= 5)
+ORDER BY category, product_total_sales DESC;
+
+
+
+-- The Second Task: Impute_missing_values
+
+WITH unit_prices AS (
+    SELECT 
+        product_id, 
+        discount, 
+        sales / quantity::NUMERIC AS unit_price
+    FROM orders
+    WHERE quantity IS NOT NULL
+)
+SELECT DISTINCT
+    o.product_id, 
+    o.discount, 
+    market, 
+    region, 
+    sales, 
+    quantity,
+    ROUND(sales::NUMERIC / unit_price) AS calculated_quantity
+FROM orders AS o
+JOIN unit_prices AS up
+   USING(product_id, discount)
+WHERE o.quantity IS NULL
+ORDER BY o.product_id;
+
+
+
+
---
+++ Analyzing and Formatting PostgreSQL Sales Data/README.md
@@ -0,0 +1,32 @@
+Analyzing and Formatting PostgreSQL Sales Data.
+	
+Cleaning incorrect data types and missing values.
+
+Project Description:
+	Work with superstore data in PostgreSQL, reformatting and analyzing to answer some day-to-day retail business questions,
+	like what are the top performing products, and how missing data can be imputed for quantity of products per order.
+
+	Task1: 
+		Find the top 5 products from each category based on highest total sales. The output should be sorted by category in ascending order and by sales 
+		in descending order within each category, i.e. within each category product with highest margin should sit on the top. Include the following columns in your data:
+		
+			category, 
+			product_name, 
+			product_total_sales (rounded to two decimal places), 
+			product_total_profit (rounded to two decimal places), 
+			product_rank
+			
+	
+	Task2: 
+		Calculate the quantity for orders with missing values in the quantity column. To do this, first calculate the unit price for each product 
+		based on the orders where the quantity is available, considering factors that might affect pricing. Then, use this unit price to estimate 
+		the missing quantity values for orders where the quantity is missing. The calculated values should be stored in the calculated_quantity column. 
+		Include the following columns in your data:
+		
+			product_id, 
+			discount, 
+			market, 
+			region, 
+			sales, 
+			quantity, 
+			calculated_quantity (rounded to zero decimal places)
